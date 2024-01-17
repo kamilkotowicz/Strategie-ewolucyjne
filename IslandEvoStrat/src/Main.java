582b870022471acaa21a7985d0ae50789c11afbe
kamilkotowicz <58431435+kamilkotowicz@users.noreply.github.com>
2024-01-17 12:42:03 +0100

    Podstawowa wersja algorytmu

--- IslandEvoStrat/src/Main.java
+++ IslandEvoStrat/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        final int POPULATION_SIZE = 30;
+        final int DIMENSION = 10;
+        final int ITERATIONS = 1000;
+        final int CHILD_SIZE = 200;
+
+        EvolutionaryAlgorithm algo = new EvolutionaryAlgorithm(POPULATION_SIZE, DIMENSION, ITERATIONS, CHILD_SIZE);
+        algo.initializePopulation();
+
+        for (int iteration = 0; iteration < ITERATIONS; iteration++) {
+            algo.generateChildren();
+            algo.selectSurvivors();
+        }
+
+        double[] bestIndividual = algo.getBestIndividual();
+        System.out.println("Best individual: " + Arrays.toString(bestIndividual));
     }
 }
